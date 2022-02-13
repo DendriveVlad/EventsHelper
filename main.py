ee1eddbf9a2c235b73506a16035bc4cf03b81991
DendriveVlad <dendrivevlad@gmail.com>
2022-02-13 21:33:33 +0500

    crash fix

--- main.py
+++ main.py
@@ -116,7 +116,10 @@ class Bot(commands.Bot):
                     await voice.delete(reason="Ивент окончен")
                 except AttributeError:
                     pass
-                mess = await utils.get(self.get_guild(GUILD_ID).text_channels, id=CHANNELS["Schedule"]).fetch_message(event["mention"])
+                try:
+                    mess = await utils.get(self.get_guild(GUILD_ID).text_channels, id=CHANNELS["Schedule"]).fetch_message(event["mention"])
+                except AttributeError:
+                    pass
                 mess2 = await utils.get(self.get_guild(GUILD_ID).text_channels, id=CHANNELS["Events_list"]).fetch_message(event["message_id"])
                 await mess.delete()
                 await mess2.delete()
